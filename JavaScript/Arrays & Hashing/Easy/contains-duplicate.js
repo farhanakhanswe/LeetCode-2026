d41fe11ecb42825212a82b18c363446eb3ca50f3
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2026-01-01 23:33:37 +0600

    Update contains-duplicate.js

--- JavaScript/Arrays & Hashing/Easy/contains-duplicate.js
+++ JavaScript/Arrays & Hashing/Easy/contains-duplicate.js
@@ -4,22 +4,15 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    
     if(nums.length < 2) return false;
 
     let map = new Map();
 
     for(let i = 0; i < nums.length ; i++){
         if(map.has(nums[i])){
-            map.set(nums[i], map.get(nums[i]) + 1)
+            return true;
         }else{
             map.set(nums[i], 1)
-        }
-    }
-
-    for (const value of map.values()) {
-        if (value > 1) {
-            return true;
         }
     }
 
