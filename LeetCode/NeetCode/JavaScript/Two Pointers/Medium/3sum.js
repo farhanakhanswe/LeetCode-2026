71bf805b90fd040e845b85963c826146ba89279f
farhanakhanswe <farhanakhanswe@gmail.com>
2026-02-04 23:54:35 +0600

    js soln for 3sum updated

--- LeetCode/NeetCode/JavaScript/Two Pointers/Medium/3sum.js
+++ LeetCode/NeetCode/JavaScript/Two Pointers/Medium/3sum.js
@@ -4,27 +4,37 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-    nums.sort((a, b) => a - b);
+    nums.sort((a, b) => a - b); // sort the nums arr in ascending order
     let result = []
 
-    for (let i = 0; i < nums.length; i++) {
+    // loop doesn't need to run all the way to end of the loop as we need 3 elements for each triplet
+    for (let i = 0; i < nums.length - 2; i++) {
+
+        // to avoid duplicates
         if (i > 0 && nums[i] == nums[i - 1]) {
             continue;
         }
 
-        leftPtr = i + 1;
-        rightPtr = nums.length - 1;
+        // taking the two pointer approach
+        let leftPtr = i + 1;
+        let rightPtr = nums.length - 1;
+
+        // using a similar concept to what we used in 2sum problem
+        // the goal is that the elemnts in the triplet must add to 0
+        // if sum > 0, then we decrease the total sum by decreasing the number from the right since the rightmost numbers are larger than the leftmost
+        // if sum < 0, then we increase the total sum by increasing the number from the left since the leftmost numbers are smaller than the rightmost 
 
         while (leftPtr < rightPtr) {
-            let threeSum = nums[i] + nums[leftPtr] + nums[rightPtr];
-            if (threeSum > 0) {
+            let totalSum = nums[i] + nums[leftPtr] + nums[rightPtr];
+            if (totalSum > 0) {
                 rightPtr -= 1;
-            } else if (threeSum < 0) {
+            } else if (totalSum < 0) {
                 leftPtr += 1;
             } else {
                 result.push([nums[i], nums[leftPtr], nums[rightPtr]]);
                 leftPtr += 1;
 
+                // to avoid duplicates
                 while (nums[leftPtr] == nums[leftPtr - 1] && leftPtr < rightPtr) {
                     leftPtr += 1
                 }
