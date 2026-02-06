c810526b8b9b91fe87f84aca63c46f9112c98fcf
farhanakhanswe <farhanakhanswe@gmail.com>
2026-02-06 23:58:57 +0600

    js soln for two sum two pointer technique updated

--- LeetCode/NeetCode/JavaScript/Arrays & Hashing/Easy/two-sum.js
+++ LeetCode/NeetCode/JavaScript/Arrays & Hashing/Easy/two-sum.js
@@ -36,9 +36,17 @@ var twoSum = function(nums, target) {
         return [-1, -1];
     }
 
-    const sortedArr = [...nums].sort((a,b) => a - b);
+    const sortedArr = [...nums].sort((a,b) => a - b); // sort the array
     let firstPtr = 0;
     let lastPtr = nums.length - 1;
+
+    // since the array is sorted we know the numbers on the left will be smaller than the ones on the right
+    // we can use pointer technique to move from left and right towards eachother
+    // we can add the numbers and check if the sum is greater or less than 0
+    // if the sum is less than the target, we have to increase one of the numbers to increase the sum 
+    // in this case, we increase number by moving the left pointer to the right
+    // if the sum is more than the target, we have to decrease the sum by reducing one of the numbers
+    // in this case, we decrease the number by moving the right pointer to left
 
     while(firstPtr < lastPtr){
         let sum = sortedArr[firstPtr] + sortedArr[lastPtr];
