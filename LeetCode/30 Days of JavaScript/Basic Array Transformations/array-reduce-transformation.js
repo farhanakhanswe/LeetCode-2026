2b2448d9d22c2638848bf92fdb25a12c87623ee5
farhanakhanswe <farhanakhanswe@gmail.com>
2026-03-13 00:59:05 +0600

    Update solution for array reduce transformation problem with comments

--- LeetCode/30 Days of JavaScript/Basic Array Transformations/array-reduce-transformation.js
+++ LeetCode/30 Days of JavaScript/Basic Array Transformations/array-reduce-transformation.js
@@ -1,11 +1,12 @@
 /**
- * Link: https://leetcode.com/problems/array-reduce-transformation/?envType=study-plan-v2&envId=30-days-of-javascript
  * @param {number[]} nums
  * @param {Function} fn
  * @param {number} init
  * @return {number}
  */
 var reduce = function (nums, fn, init) {
+    // what I have to do here is pass "init" as the first argument the first time
+    // and afterwards, for each iteration, first argument will be the result of the calculation done on the preceding element in the array
     let result = init;
 
     for (let i = 0; i < nums.length; i++) {
@@ -14,3 +15,4 @@ var reduce = function (nums, fn, init) {
 
     return result;
 };
+
