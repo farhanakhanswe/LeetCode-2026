0d9b7fb68fb559d28b8a6b1e1533ba5a935bdf0c
farhanakhanswe <farhanakhanswe@gmail.com>
2026-03-19 00:43:42 +0600

    Update comments of remove element

--- LeetCode/NeetCode/Arrays & Hashing/Easy/remove-element/remove-element.js
+++ LeetCode/NeetCode/Arrays & Hashing/Easy/remove-element/remove-element.js
@@ -5,27 +5,33 @@
  * @return {number}
  */
 var removeElement = function (nums, val) {
-    // understanding the problem //
+    // Understanding the problem //
 
     // i have an integer array nums
     // integer val
     // i have to remove all occurrences of val in nums in place
     // the order of the elements may be changed
-    // i have to return the number of eleents in nums which are not equal to val
+    // i have to return the number of elements in nums which are not equal to val
     // consider the number of elements in nums which are not equal to val be k, to get accepted, i need to do the following
     // (1) change the array nums such that the first k elements of nums contain the elements which are not equal to val
-    // the remaning elements of nums are not important as well as the size of nums
+    // the remaining elements of nums are not important as well as the size of nums
     // i have to return k
 
-    // planning //
+    // Planning //
 
-    // okay so we wont remove the non-val numbers, we have to rearrange the elements in such a way that the first k elements of nums
+    // We have to rearrange the elements in the array in such a way that the first k elements of nums
     // contain the elements which are not equal to val
+    // k is an integer. By first k elements of nums, we mean let's say first 5 elements of nums
 
-    // lets say the total number of non-val numbers in nums is 3 so we have to rearrange the array in such a way that 
-    // from index 0 till the k, all the numbers are not val. 
+    // Let's say the total number of non-val numbers in nums is 3 so we have to rearrange the array in such a way that 
+    // from index 0 till the (k - 1), all the numbers are not val.
+    // It doesn't matter what comes after k elements even if the next numbers contain numbers that equal to val
+    // Example: [1,2,3,4,4,3] and val = 3
+    // After rearrangement: [1,2,4,4,4,3] and k = 4 
+    // So, we are only concerned with the first 4 elements in nums
+    // It doesn't matter that 3 appeared after k elements as the question isn't bothered by that
 
-    // execution
+    // Execution
 
     let k = 0;
 
