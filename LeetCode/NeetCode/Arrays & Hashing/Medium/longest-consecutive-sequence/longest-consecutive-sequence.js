b081ba640d6f04ee8d4d6ca1af6012da7d73cf26
farhanakhanswe <farhanakhanswe@gmail.com>
2026-03-20 23:54:07 +0600

    Update js soln for longest consecutive sequence

--- LeetCode/NeetCode/Arrays & Hashing/Medium/longest-consecutive-sequence/longest-consecutive-sequence.js
+++ LeetCode/NeetCode/Arrays & Hashing/Medium/longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -1,7 +1,11 @@
 /**
+ * Question Link: https://leetcode.com/problems/longest-consecutive-sequence/
  * @param {number[]} nums
  * @return {number}
  */
+
+/* Solution 1: Time complexity: O(n log n)  */
+
 var longestConsecutive = function (nums) {
     if (nums.length === 0) return 0;
 
@@ -18,6 +22,40 @@ var longestConsecutive = function (nums) {
         }
 
         longest = Math.max(longest, count);
+    }
+
+    return longest;
+
+};
+
+/* Solution 2 : Time Complexity: O(n) */
+
+var longestConsecutive = function (nums) {
+    if (nums.length === 0) return 0;
+
+    const set = new Set(nums); // we do this to avoid duplicate values and for fast lookup O(1)
+    let longest = 0;
+
+    // now we would look for the starting number of the longest sequence
+    // and we do this by checking if the previous number of the curr num in for loop exists
+
+    for (let num of set) {
+        if (!set.has(num - 1)) {
+            // if the set doesn't have the previous number then we know the curr num is the starting point of sequence
+            let current = num;
+            let count = 1;
+
+            // now lets find the length of the sequence
+
+            while (set.has(current + 1)) {
+                current++;
+                count++;
+            }
+
+            // now lets find the longest sequence 
+
+            longest = Math.max(longest, count);
+        }
     }
 
     return longest;
