1b8bec9c58760832e5a6d053de57f41579ed59a4
farhanakhanswe <farhanakhanswe@gmail.com>
2026-03-23 22:59:00 +0600

    Update js soln fro product of array except self

--- LeetCode/NeetCode/Arrays & Hashing/Medium/product-of-array-except-self/product-of-array-except-self.js
+++ LeetCode/NeetCode/Arrays & Hashing/Medium/product-of-array-except-self/product-of-array-except-self.js
@@ -1,30 +1,50 @@
 /**
+ * Question Link: https://leetcode.com/problems/product-of-array-except-self/
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function(nums) {
-    // given an int arr nums, return an arr answer such that answer[i] = the product of all el of nums except nums[i]
 
-    // okay lets not think about O(n) for now
-    // if i use a nested for loop to find the product of all els except curr el, that'll give the answer
+// Solution 1: Time Complexity is O(n) and Space Complexity is O(n). I'll try to find a soln that'll make the space complexity O(1).
+var productExceptSelf = function (nums) {
+    // lets say we traverse through the nums in a for loop
+    // and we have already kept a track of the product of all the elements before and after the curr el
+    // then all we have to do is multiply product of all the elements before curr el with all the elements after the curr el
+    // we can keep two maps where the key would be index and value would be product of all el from index 0 to curr index
+    // in another map, the key would be index and the value would be the product of all the el from last el to curr index
 
-    let result = [];
+    let leftMap = new Map();
+    let rightMap = new Map();
+    let product = 1;
+    let output = [];
 
-    for(let i = 0; i < nums.length; i++){
-        let product = 1;
-        for(let j = 0; j < nums.length; j++){
-            if(i !== j){
-                product = product * nums[j];
-            }
-        }
-
-        result.push(product);
+    for (let i = 0; i < nums.length; i++) {
+        product *= nums[i];
+        leftMap.set(i, product);
     }
 
-    return result;
+    product = 1;
 
-    // now lets think of an optimized solution
-    // lets say if i know the product of all el
-    // as lets say the current el is nums[i] and if we know the product of all the elements till nums[i - 1]
-    
+    for (let i = nums.length - 1; i >= 0; i--) {
+        product *= nums[i];
+        rightMap.set(i, product);
+    }
+
+    product = 1;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (leftMap.has(i - 1) && rightMap.has(i + 1)) {
+            product = leftMap.get(i - 1) * rightMap.get(i + 1);
+        } else if (!leftMap.has(i - 1) && rightMap.has(i + 1)) {
+            product = rightMap.get(i + 1);
+        } else if (leftMap.has(i - 1) && !rightMap.has(i + 1)) {
+            product = leftMap.get(i - 1);
+        }
+
+        output.push(product);
+
+    }
+
+    return output;
+
+
 };
