4e33e31b26a98c97766982a3a15fbf398a5b1a35
farhanakhanswe <farhanakhanswe@gmail.com>
2026-04-08 23:38:14 +0600

    Add js soln for maximum subarray

--- LeetCode/NeetCode/Arrays & Hashing/Medium/maximum-subarray/maximum-subarray.js
+++ LeetCode/NeetCode/Arrays & Hashing/Medium/maximum-subarray/maximum-subarray.js
@@ -39,3 +39,27 @@ var maxSubArray = function (nums) {
 
     return maxSum;
 };
+
+
+// Solution 3:
+
+var maxSubArray = function (nums) {
+    let maxSum = nums[0];
+    let curSum = 0;
+    let maxL = 0, maxR = 0;
+    let L = 0;
+
+    for (let R = 0; R < nums.length; R++) {
+        if (curSum < 0) {
+            curSum = 0;
+            L = R;
+        }
+        curSum += nums[R];
+        if (curSum > maxSum) {
+            maxSum = curSum;
+            maxL = L;
+            maxR = R;
+        }
+    }
+    return maxSum;
+};
