d0b68e245d68403be73bc9c5e67923070803c331
farhanakhanswe <farhanakhanswe@gmail.com>
2026-04-17 23:52:14 +0600

    Add js soln for promise time limit

---
+++ LeetCode/30 Days of JavaScript/Promises and Time/promise-time-limit.js
@@ -0,0 +1,32 @@
+/**
+ * Question Link: https://leetcode.com/problems/promise-time-limit/?envType=study-plan-v2&envId=30-days-of-javascript
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function (fn, t) {
+
+    return async function (...args) {
+        return new Promise((resolve, reject) => {
+
+            const timer = setTimeout(() => {
+                reject("Time Limit Exceeded");
+            }, t);
+
+            fn(...args)
+                .then((result) => {
+                    clearTimeout(timer);
+                    resolve(result);
+                })
+                .catch((err) => {
+                    clearTimeout(timer);
+                    reject(err);
+                });
+        });
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
