99d715f1d8281989bcc8de2a71ae52f81301b66d
farhanakhanswe <farhanakhanswe@gmail.com>
2026-05-15 22:59:30 +0600

    Update notes on counter-ii.js

--- LeetCode/30 Days of JavaScript/Closures/counter-ii.js
+++ LeetCode/30 Days of JavaScript/Closures/counter-ii.js
@@ -28,6 +28,19 @@ var createCounter = function (init) {
 /* 
     Notes:
 
+    This question is a good way to test how much you've already learned from the other questions under the 
+    Closures section. Please take a look at their notes so that you can fully grasp what's happening here.
+    
+    Once that's done, let's move forward to the following:
+    
     (1) We have to store the initial value of init in a variable so that we can later use it for resetting
+    (2) The returned object will contain three closures as properties and we can call each of these closures using the
+        object's dot notation
+        
+   
+        const counter = createCounter(5)
+        counter.increment(); // 6
+        counter.reset(); // 5
+        counter.decrement(); // 4
 
 */
