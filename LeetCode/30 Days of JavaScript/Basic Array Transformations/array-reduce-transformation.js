3c3021a458dfcc5eec8c52d7d614874e4ba899c7
farhanakhanswe <farhanakhanswe@gmail.com>
2026-05-19 23:51:45 +0600

    Add notes to array-reduce-transformation.js

--- LeetCode/30 Days of JavaScript/Basic Array Transformations/array-reduce-transformation.js
+++ LeetCode/30 Days of JavaScript/Basic Array Transformations/array-reduce-transformation.js
@@ -5,8 +5,6 @@
  * @return {number}
  */
 var reduce = function (nums, fn, init) {
-    // what I have to do here is pass "init" as the first argument the first time
-    // and afterwards, for each iteration, first argument will be the result of the calculation done on the preceding element in the array
     let result = init;
 
     for (let i = 0; i < nums.length; i++) {
@@ -16,3 +14,29 @@ var reduce = function (nums, fn, init) {
     return result;
 };
 
+/* 
+    Notes:
+
+    (1) Let's think of a bit more simplified solution first:
+
+        var reduce = function(nums, fn, init) {
+            if(nums.length === 0) return init;
+    
+            let result = fn(init, nums[0]);
+
+            for(let i = 1 ; i < nums.length; i++){
+                result = fn(result, nums[i]);
+            }
+
+            return result;
+        };
+
+        Here, we are returning init for empty arrays as it's a requirement.
+        Then, we are calling the callback function "fn", passing init and the first element as arguments to it,
+        to get our initial result.
+        Then, we use a for loop that starts from the index = 1 element which is the second element, and on every 
+        iteration, we call the callback function fn and pass the current element and the value returned from the 
+        last fn call as arguments.
+
+
+*/
