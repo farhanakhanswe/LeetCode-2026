183823c6d9dd068f3f9b3fc9202ee09d353ef14b
farhanakhanswe <farhanakhanswe@gmail.com>
2026-05-20 22:06:45 +0600

    Add notes to function-composition.js

--- LeetCode/30 Days of JavaScript/Function Transformations/function-composition.js
+++ LeetCode/30 Days of JavaScript/Function Transformations/function-composition.js
@@ -1,17 +1,19 @@
+// Question Link: https://leetcode.com/problems/function-composition/?envType=study-plan-v2&envId=30-days-of-javascript
+
 /**
- * Question Link: https://leetcode.com/problems/function-composition/?envType=study-plan-v2&envId=30-days-of-javascript
  * @param {Function[]} functions
  * @return {Function}
  */
 var compose = function (functions) {
+
     return function (x) {
-        let output = x;
+        let result = x;
 
         for (let i = functions.length - 1; i >= 0; i--) {
-            output = functions[i](output)
+            result = functions[i](result);
         }
 
-        return output;
+        return result;
     }
 };
 
@@ -19,3 +21,17 @@ var compose = function (functions) {
  * const fn = compose([x => x + 1, x => 2 * x])
  * fn(4) // 9
  */
+
+/*
+
+    (1) Function composition is the process of combining two or more functions so that the output of one function
+        becomes the input of another.
+
+    (2) The question says that the function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))). This means
+        the input of g(x) is the result returned from h(x). Then, the input of f(x) will be the result returned from
+        g(x). This means we have to loop over the functions array from right to left. 
+        
+    (3) The question also mentions that the function composition of an empty list of functions 
+        is the identity function f(x) = x which simply means that if the array is empty we will just return the x value
+
+*/
